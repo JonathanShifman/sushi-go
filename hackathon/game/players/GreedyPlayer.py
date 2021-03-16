4c4ea797dd38bfe315b43f4c1d58e33b1f82ed24
Jonathan <jonathan.shifman@gmail.com>
2021-03-17 00:07:47 +0200

    Commented out if

--- hackathon/game/players/GreedyPlayer.py
+++ hackathon/game/players/GreedyPlayer.py
@@ -18,8 +18,8 @@ class GreedyPlayer(object):
     def play(self, game_knowledge) -> list:
         evaluations_list = self.evaluate_hand(extract_current_hand(game_knowledge))
         best_card_index = int(argmax(evaluations_list))
-        if has_chopsticks_in_plate(extract_current_plate(game_knowledge)):
-            return [int(argmax(
-                evaluations_list[0:best_card_index] + evaluations_list[best_card_index + 1: len(evaluations_list)])
-            )] + [best_card_index]
+        # if has_chopsticks_in_plate(extract_current_plate(game_knowledge)):
+        #     return [int(argmax(
+        #         evaluations_list[0:best_card_index] + evaluations_list[best_card_index + 1: len(evaluations_list)])
+        #     )] + [best_card_index]
         return [best_card_index]
